28a6ace7d605cddcaeb5151cd1689d7477a04b9a
Steve Dekorte <steve@dekorte.com>
2014-04-16 12:18:25 -0700

    made Kit header file, made headers public

--- BitmessageKit/BitmessageKit.h
+++ BitmessageKit/BitmessageKit.h
@@ -8,6 +8,73 @@
 
 #import <Foundation/Foundation.h>
 
-@interface BitmessageKit : NSObject
 
-@end
+#import "BMNode.h"
+
+#import "NSMutableAttributedString+extra.h"
+#import "BMMessages.h"
+#import "BMSubscription.h"
+#import "BMChannels.h"
+#import "BMAddressed.h"
+#import "NSString+BM.h"
+#import "BMIdentities.h"
+#import "BMClient.h"
+#import "BMAddress.h"
+#import "BMKeysFile.h"
+#import "BMReceivedMessage.h"
+#import "BMMessage.h"
+#import "BMArchive.h"
+#import "BMChannel.h"
+#import "BMProxyMessage.h"
+#import "NSArray+extra.h"
+#import "BMDatabase.h"
+#import "NSFileManager+DirectoryLocations.h"
+#import "NSObject+extra.h"
+#import "NSArray+extra.h"
+#import "BMIdentity.h"
+#import "BMMessageGroup.h"
+#import "BMSubscriptions.h"
+#import "BMSentMessages.h"
+#import "BMContacts.h"
+#import "BMServerProcess.h"
+#import "NSDictionary+json.h"
+#import "BMNode.h"
+#import "BMReceivedMessages.h"
+#import "BMContact.h"
+#import "BMSentMessage.h"
+#import "JSONDB.h"
+
+/*
+Vendor/ZipKit/ZipKit/NSFileManager+ZKAdditions.h
+Vendor/ZipKit/ZipKit/ZKDataArchive.h
+Vendor/ZipKit/ZipKit/ZipKit-Prefix.pch
+Vendor/ZipKit/ZipKit/GMAppleDouble+ZKAdditions.h
+Vendor/ZipKit/ZipKit/ZKCDTrailer64Locator.h
+Vendor/ZipKit/ZipKit/ZKFileArchive.h
+Vendor/ZipKit/ZipKit/ZKCDTrailer64.h
+Vendor/ZipKit/ZipKit/NSDate+ZKAdditions.h
+Vendor/ZipKit/ZipKit/ZKDefs.h
+Vendor/ZipKit/ZipKit/MacFUSE/GMAppleDouble.h
+Vendor/ZipKit/ZipKit/NSString+ZKAdditions.h
+Vendor/ZipKit/ZipKit/ZKLFHeader.h
+Vendor/xmlrpc/XMLRPCRequest.h
+Vendor/xmlrpc/XMLRPCResponse.h
+Vendor/ZipKit/ZipKit/NSData+ZKAdditions.h
+Vendor/ZipKit/ZipKit/ZKLog.h
+Vendor/ZipKit/ZipKit/NSFileHandle+ZKAdditions.h
+Vendor/xmlrpc/NSStringAdditions.h
+Vendor/xmlrpc/XMLRPCConnectionDelegate.h
+Vendor/xmlrpc/XMLRPCEventBasedParser.h
+Vendor/xmlrpc/NSData+Base64.h
+Vendor/xmlrpc/XMLRPC.h
+Vendor/ZipKit/ZipKit/ZKCDTrailer.h
+Vendor/ZipKit/ZipKit/ZKCDHeader.h
+Vendor/xmlrpc/XMLRPCEventBasedParserDelegate.h
+Vendor/ZipKit/ZipKit/ZipKit.h
+Vendor/ZipKit/ZipKit/ZKArchive.h
+Vendor/xmlrpc/XMLRPCConnection.h
+Vendor/xmlrpc/XMLRPCConnectionManager.h
+Vendor/xmlrpc/XMLRPCEncoder.h
+Vendor/xmlrpc/XMLRPCDefaultEncoder.h
+Vendor/ZipKit/ZipKit/NSDictionary+ZKAdditions.h
+*/
