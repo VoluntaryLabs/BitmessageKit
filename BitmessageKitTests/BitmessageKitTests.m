88bef688e87f64c90e10001fedb1c7c0d08edd24
Steve Dekorte <steve@dekorte.com>
2014-11-12 15:31:46 -0800

    cleanup

--- BitmessageKitTests/BitmessageKitTests.m
+++ BitmessageKitTests/BitmessageKitTests.m
@@ -3,7 +3,7 @@
 //  BitmessageKitTests
 //
 //  Created by Adam Thorsen on 4/9/14.
-//  Copyright (c) 2014 Adam Thorsen. All rights reserved.
+//  Copyright (c) 2014 voluntary.net. All rights reserved.
 //
 
 #import <XCTest/XCTest.h>
